f52fecb30816d7615f0a0dd86fc6250e4bbf173d
MijstenHove <mijs.tenhove@hotmail.com>
2022-05-23 12:08:50 +0200

    broken

--- GraphicPrograming/Grapic_Programing/Grapic_Programing/Grapic_Programing.cpp
+++ GraphicPrograming/Grapic_Programing/Grapic_Programing/Grapic_Programing.cpp
@@ -1,16 +1,19 @@
-// OpenGL_Lessen.cpp : This file contains the 'main' function. Program execution 
-
+// OpenGL_Lessen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
 #include <iostream>
 #include <glad/glad.h>
 #include "GLFW/glfw3.h"
 #include <fstream>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+glm::vec3 cameraPosition(0, 5, 0), cameraForward(0, 0, 1), cameraUp(0, 1, 0);
 
 void loadFromFile(const char* url, char** target) {
     std::ifstream stream(url, std::ios::binary);
@@ -22,15 +25,18 @@ void loadFromFile(const char* url, char** target) {
     (*target)[total] = '\0';
     stream.close();
 }
+
 unsigned int loadTexture(std::string url, GLenum format) {
     // gen & bind IDs
     unsigned int textureID;
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
+
     // instellen texture params
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    // shit inladen
+
+    // inladen
     int width, height, channels;
     unsigned char* data;
     data = stbi_load(url.c_str(), &width, &height, &channels, 0);
@@ -47,28 +53,206 @@ unsigned int loadTexture(std::string url, GLenum format) {
     glBindTexture(GL_TEXTURE_2D, 0);
     return textureID;
 }
+
+void CreateShader(const char* url, GLenum type, unsigned int& shader) {
+    static int success;
+    static char infoLog[512];
+
+    char* target;
+    loadFromFile(url, &target);
+
+    shader = glCreateShader(type);
+    glShaderSource(shader, 1, &target, nullptr);
+    glCompileShader(shader);
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+unsigned int GeneratePlane(const char* heightmap, GLenum format, int comp, float hScale, float xzScale, unsigned int& size, unsigned int& heightmapID) {
+    int width = 0, height = 0, channels;
+    unsigned char* data = nullptr;
+    if (heightmap != nullptr) {
+        data = stbi_load(heightmap, &width, &height, &channels, comp);
+        if (data) {
+            glGenTextures(1, &heightmapID);
+            glBindTexture(GL_TEXTURE_2D, heightmapID);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+            glBindTexture(GL_TEXTURE_2D, 0);
+        }
+    }
+    //hier
+    int stride = 8;
+    float* vertices = new float[(width * height) * stride];
+    unsigned int* indices = new unsigned int[(width - 1) * (height - 1) * 6];
+
+    int index = 0;
+    for (int i = 0; i < (width * height); i++) {
+        // TODO: calculate x/z values
+        int x = i % width;
+        int z = i / width;
+
+        // TODO: set position
+        vertices[index++] = x * xzScale;
+        vertices[index++] = 0;  //dit is waar je de pixel zou lezen...
+        vertices[index++] = z * xzScale;
+
+        // TODO: set normal
+        vertices[index++] = 0;
+        vertices[index++] = 1;
+        vertices[index++] = 0;
+
+        // TODO: set uv
+        vertices[index++] = x / (float)(width - 1);
+        vertices[index++] = z / (float)(height - 1);
+    }
+
+    // OPTIONAL TODO: Calculate normal
+    // TODO: Set normal
+
+    index = 0;
+    for (int i = 0; i < (width - 1) * (height - 1); i++) {
+        int x = i % (width - 1);
+        int z = i / (width - 1);
+
+        int vertex = z * width + x;
+
+        indices[index++] = vertex;
+        indices[index++] = vertex + width + 1;
+        indices[index++] = vertex + 1;
+
+        indices[index++] = vertex;
+        indices[index++] = vertex + width;
+        indices[index++] = vertex + width + 1;
+    }
+
+    unsigned int vertSize = (width * height) * stride * sizeof(float);
+    size = ((width - 1) * (height - 1) * 6) * sizeof(unsigned int);
+
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertSize, vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
+
+    // vertex information!
+    // position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, 0);
+    glEnableVertexAttribArray(0);
+    // normal
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (void*)(sizeof(float) * 3));
+    glEnableVertexAttribArray(1);
+    // uv
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (void*)(sizeof(float) * 6));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    delete[] vertices;
+    delete[] indices;
+
+    stbi_image_free(data);
+
+    return VAO;
+}
+
+void handleInput(GLFWwindow* window, float deltaTime) {
+    static bool w, s, a, d, space, ctrl;
+    static double cursorX = -1, cursorY = -1, lastCursorX, lastCursorY;
+    static float pitch, yaw;
+    static float speed = 100.0f;
+
+    float sensitivity = 100.0f * deltaTime;
+    float step = speed * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)				w = true;
+    else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_RELEASE)		w = false;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)				s = true;
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_RELEASE)		s = false;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)				a = true;
+    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_RELEASE)		a = false;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)				d = true;
+    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_RELEASE)		d = false;
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)				space = true;
+    else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE)		space = false;
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)		ctrl = true;
+    else if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_RELEASE)	ctrl = false;
+
+    if (cursorX == -1) {
+        glfwGetCursorPos(window, &cursorX, &cursorY);
+    }
+
+    lastCursorX = cursorX;
+    lastCursorY = cursorY;
+    glfwGetCursorPos(window, &cursorX, &cursorY);
+
+    glm::vec2 mouseDelta(cursorX - lastCursorX, cursorY - lastCursorY);
+
+    // TODO: calculate rotation & movement
+    yaw -= mouseDelta.x * sensitivity;
+    pitch += mouseDelta.y * sensitivity;
+
+    if (pitch < -90.0f) pitch = -90.0f;
+    else if (pitch > 90.0f) pitch = 90.0f;
+
+    if (yaw < -180.0f) yaw += 360;
+    else if (yaw > 180.0f) yaw -= 360;
+
+    glm::vec3 euler(glm::radians(pitch), glm::radians(yaw), 0);
+    glm::quat q(euler);
+
+    // update camera position / forward & up
+    glm::vec3 translation(0, 0, 0);
+    cameraPosition += q * translation;
+
+    cameraUp = q * glm::vec3(0, 1, 0);
+    cameraForward = q * glm::vec3(0, 0, 1);
+}
+
 int main()
 {
-    int width = 800;
-    int hight = 600;
+    static double previousT = 0;
 
     std::cout << "Hello World!\n";
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(width, hight, "Hello OpenGL!", nullptr,
-        nullptr);
+
+    int width = 800;
+    int height = 600;
+    GLFWwindow* window = glfwCreateWindow(width, height, "Hello OpenGL!", nullptr, nullptr);
     glfwMakeContextCurrent(window);
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-  
-    
-    glViewport(0, 0, width, hight);
+
+    glViewport(0, 0, width, height);
+
     ///SETUP OBJECT///
+
     // Vertices of our triangle!
     // need 24 vertices for normal/uv-mapped Cube
     float vertices[] = {
@@ -77,27 +261,38 @@ int main()
         0.5f, -0.5f, 0.5f,      1.0f, 1.0f, 0.0f,   1.f, 1.f,       0.f, -1.f, 0.f,
         -0.5f, -0.5f, 0.5f,     0.0f, 1.0f, 1.0f,   0.f, 1.f,       0.f, -1.f, 0.f,
         -0.5f, -0.5f, -.5f,     1.0f, 1.0f, 1.0f,   0.f, 0.f,       0.f, -1.f, 0.f,
+
         0.5f, 0.5f, -0.5f,      1.0f, 1.0f, 1.0f,   2.f, 0.f,       1.f, 0.f, 0.f,
         0.5f, 0.5f, 0.5f,       1.0f, 1.0f, 1.0f,   2.f, 1.f,       1.f, 0.f, 0.f,
+
         0.5f, 0.5f, 0.5f,       1.0f, 1.0f, 1.0f,   1.f, 2.f,       0.f, 0.f, 1.f,
         -0.5f, 0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   0.f, 2.f,       0.f, 0.f, 1.f,
+
         -0.5f, 0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   -1.f, 1.f,      -1.f, 0.f, 0.f,
         -0.5f, 0.5f, -.5f,      1.0f, 1.0f, 1.0f,   -1.f, 0.f,      -1.f, 0.f, 0.f,
+
         -0.5f, 0.5f, -.5f,      1.0f, 1.0f, 1.0f,   0.f, -1.f,      0.f, 0.f, -1.f,
         0.5f, 0.5f, -0.5f,      1.0f, 1.0f, 1.0f,   1.f, -1.f,      0.f, 0.f, -1.f,
+
         -0.5f, 0.5f, -.5f,      1.0f, 1.0f, 1.0f,   3.f, 0.f,       0.f, 1.f, 0.f,
         -0.5f, 0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   3.f, 1.f,       0.f, 1.f, 0.f,
+
         0.5f, -0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   1.f, 1.f,       0.f, 0.f, 1.f,
         -0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 1.0f,   0.f, 1.f,       0.f, 0.f, 1.f,
+
         -0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 1.0f,   0.f, 1.f,       -1.f, 0.f, 0.f,
         -0.5f, -0.5f, -.5f,     1.0f, 1.0f, 1.0f,   0.f, 0.f,       -1.f, 0.f, 0.f,
+
         -0.5f, -0.5f, -.5f,     1.0f, 1.0f, 1.0f,   0.f, 0.f,       0.f, 0.f, -1.f,
         0.5f, -0.5f, -0.5f,     1.0f, 1.0f, 1.0f,   1.f, 0.f,       0.f, 0.f, -1.f,
+
         0.5f, -0.5f, -0.5f,     1.0f, 1.0f, 1.0f,   1.f, 0.f,       1.f, 0.f, 0.f,
         0.5f, -0.5f, 0.5f,      1.0f, 1.0f, 1.0f,   1.f, 1.f,       1.f, 0.f, 0.f,
+
         0.5f, 0.5f, -0.5f,      1.0f, 1.0f, 1.0f,   2.f, 0.f,       0.f, 1.f, 0.f,
         0.5f, 0.5f, 0.5f,       1.0f, 1.0f, 1.0f,   2.f, 1.f,       0.f, 1.f, 0.f
     };
+
     unsigned int indices[] = {  // note that we start from 0!
         // DOWN
         0, 1, 2,   // first triangle
@@ -118,56 +313,69 @@ int main()
         22, 12, 13,   // first triangle
         22, 13, 23,    // second triangle
     };
+
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     unsigned int EBO;
     glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-        GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     int stride = sizeof(float) * 11;
+
     // position
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, 0);
     glEnableVertexAttribArray(0);
     // color
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(float) *
-        3));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(float) * 3));
     glEnableVertexAttribArray(1);
     // uv
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(float) *
-        6));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(float) * 6));
     glEnableVertexAttribArray(2);
     // normal
-    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(float) *
-        8));
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, stride, (void*)(sizeof(float) * 8));
     glEnableVertexAttribArray(3);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     ///END SETUP OBJECT///
+
+    unsigned int size, heightmapID;
+    unsigned int plane = GeneratePlane("Heightmap2.png", GL_RGBA, 4, 1.0f, 1.0f, size, heightmapID);
+
     ///SETUP SHADER PROGRAM///
+    stbi_set_flip_vertically_on_load(true);
+
     char* vertexSource;
     loadFromFile("VerShader.shader", &vertexSource);
     char* fragmentSource;
-    loadFromFile("fragShader.shader", &fragmentSource);
+    loadFromFile("fragmentShader.shader", &fragmentSource);
+
+    unsigned int vertSky, fragSky;
+    CreateShader("VerShaderSKY.shader", GL_VERTEX_SHADER, vertSky);
+    CreateShader("fragShaderSKY.shader", GL_FRAGMENT_SHADER, fragSky);
+
     // LOAD & CREATE TEXTURES
 
     //unsigned int diffuseTexID = loadTexture("container2.png", GL_RGBA);
     unsigned int diffuseTexID = loadTexture("char.JPG", GL_RGB);
-   
+
     // END
+
     unsigned int vertID, fragID;
     vertID = glCreateShader(GL_VERTEX_SHADER);
     fragID = glCreateShader(GL_FRAGMENT_SHADER);
-  
+
     glShaderSource(vertID, 1, &vertexSource, nullptr);
     glShaderSource(fragID, 1, &fragmentSource, nullptr);
-    
+
     int success;
     char infoLog[512];
 
@@ -176,76 +384,105 @@ int main()
     if (!success)
     {
         glGetShaderInfoLog(vertID, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog <<
-            std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     };
+
     glCompileShader(fragID);
     glGetShaderiv(fragID, GL_COMPILE_STATUS, &success);
     if (!success)
     {
         glGetShaderInfoLog(fragID, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog <<
-            std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"<< infoLog << std::endl;
     };
+
     unsigned int myProgram = glCreateProgram();
-   
     glAttachShader(myProgram, vertID);
     glAttachShader(myProgram, fragID);
     glLinkProgram(myProgram);
-   
+
+    unsigned int skyProgram = glCreateProgram();
+    glAttachShader(skyProgram, vertSky);
+    glAttachShader(skyProgram, fragSky);
+    glLinkProgram(skyProgram);
+
     glDeleteShader(vertID);
     glDeleteShader(fragID);
-  
+    glDeleteShader(vertSky);
+    glDeleteShader(fragSky);
+
     ///END SETUP SHADER PROGRAM///
 
-    // matrix setup
-
-    //open gl setting 
-
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
+    /// MATRIX SETUP ///
     glUseProgram(myProgram);
+
     int worldLoc = glGetUniformLocation(myProgram, "world");
     int viewLoc = glGetUniformLocation(myProgram, "view");
-    int projnLoc = glGetUniformLocation(myProgram, "projection");
+    int projLoc = glGetUniformLocation(myProgram, "projection");
+
+    /// END MATRIX SETUP ///
+
+    // OPENGL SETTINGS //
+
+    glEnable(GL_CULL_FACE);
 
     while (!glfwWindowShouldClose(window)) {
-        double t = glfwGetTime() * 1;
-        float a = (float)sin(t * 0.0f);
-        float r = (float)sin(t * 1.0f);
-        float g = (float)cos(t * 0.0f);
-        float b = (float)tan(t * 0.0f);
+        double t = glfwGetTime();
+        float deltaTime = t - previousT;
+        previousT = t;
+        float r = (float)sin(t * 1.1f);
+        float g = (float)sin(t * 1.3f);
+        float b = (float)sin(t * 1.7f);
 
+        handleInput(window, deltaTime);
 
-        glm::mat4 world = glm::mat4(1.f);
-        world = glm::rotate(world, glm::radians((float)t * 45.0f), glm::vec3(0, 1, 1));
-        world = glm::scale(world, glm::vec3(1, 1, 1));
-        world = glm::translate(world, glm::vec3(0, 0, 0));
+        glm::mat4 view = glm::lookAt(cameraPosition, cameraPosition + cameraForward, cameraUp);
+        glm::mat4 projection = glm::perspective(glm::radians(65.0f), width / (float)height, 0.1f, 1000.0f);
 
-        glm::mat4 view = glm::lookAt(glm::vec3(0, 3, -3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-        glm::mat4 projection = glm::perspective(glm::radians(65.0f), width / (float)hight, 0.1f, 100.0f);
-
-
-        glUniformMatrix4fv(worldLoc, 1, GL_FALSE, glm::value_ptr(world));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projnLoc, 1, GL_FALSE, glm::value_ptr(projection));
-     
-     
         // iets tekenen
-        glClearColor(r, g, b, a);
-        glfwSwapBuffers(window);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, diffuseTexID);
+        // SKY BOX
+        glUseProgram(skyProgram);
+        glCullFace(GL_FRONT);
+
+        glm::mat4 world = glm::mat4(1.f);
+        world = glm::translate(world, cameraPosition);
+
+        glUniformMatrix4fv(glGetUniformLocation(skyProgram, "world"), 1, GL_FALSE, glm::value_ptr(world));
+        glUniformMatrix4fv(glGetUniformLocation(skyProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(skyProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniform3fv(glGetUniformLocation(skyProgram, "cameraPosition"), 1, glm::value_ptr(cameraPosition));
+
+        //glActiveTexture(GL_TEXTURE0);
+        //glBindTexture(GL_TEXTURE_2D, diffuseTexID);
 
         glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 6);
         glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
-       
+
+        // TERRAIN
+        glUseProgram(myProgram);
+        glCullFace(GL_BACK);
+
+        world = glm::mat4(1.f);
+        world = glm::translate(world, glm::vec3(0, 0, 0));
+
+        glUniformMatrix4fv(glGetUniformLocation(myProgram, "world"), 1, GL_FALSE, glm::value_ptr(world));
+        glUniformMatrix4fv(glGetUniformLocation(myProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(myProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        glUniform3fv(glGetUniformLocation(myProgram, "cameraPosition"), 1, glm::value_ptr(cameraPosition));
+
+        //glActiveTexture(GL_TEXTURE0);
+        //glBindTexture(GL_TEXTURE_2D, diffuseTexID);
+
+        glBindVertexArray(plane);
+        glDrawElements(GL_LINES, size, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     glfwTerminate();
     return 0;
 }
